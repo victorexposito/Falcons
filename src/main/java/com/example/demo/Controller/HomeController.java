36424a53b470f90c2ef133eec3b6aa5330d3686f
victor <victorexposito@hotmail.dk>
2019-03-05 14:10:47 +0100

    Lavet Model klass, og fyldt activity ind

---
+++ src/main/java/com/example/demo/Controller/HomeController.java
@@ -0,0 +1,18 @@
+package com.example.demo.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+
+    @GetMapping("/")
+    public String home(Model model){
+
+        return "index";
+    }
+
+
+}
