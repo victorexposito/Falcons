bc620b159430fb0bfd208feb21c8086d2780a87d
victor <victorexposito@hotmail.dk>
2019-03-05 15:48:33 +0100

    Lavet css og index side

--- src/main/java/com/example/demo/Controller/HomeController.java
+++ src/main/java/com/example/demo/Controller/HomeController.java
@@ -1,17 +1,33 @@
 package com.example.demo.Controller;
 
+import com.example.demo.Model.*;
+import com.example.demo.service.ActivityService;
+import com.example.demo.service.ServiceI;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
 public class HomeController {
 
+    @Autowired
+    ServiceI activityService;
 
     @GetMapping("/")
     public String home(Model model){
 
         return "index";
+    }
+
+    @GetMapping("/activities")
+    public String showAllActivities(Model model){
+        List<Activity> activities= activityService.readAll();
+        model.addAttribute("activityList", activities);
+
+        return "activities";
     }
 
 
