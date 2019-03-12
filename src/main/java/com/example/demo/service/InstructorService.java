92246c3eb271adcfc4b164d514d40142789840e0
victor <victorexposito@hotmail.dk>
2019-03-12 12:37:56 +0100

    Lavet css og index side

---
+++ src/main/java/com/example/demo/service/InstructorService.java
@@ -0,0 +1,34 @@
+package com.example.demo.service;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class InstructorService implements ServiceI {
+
+    @Override
+    public Object create(Object o) {
+        return null;
+    }
+
+    @Override
+    public Object read(Object o) {
+        return null;
+    }
+
+    @Override
+    public Object update(Object o) {
+        return null;
+    }
+
+    @Override
+    public Object delete(Object o) {
+        return null;
+    }
+
+    @Override
+    public List readAll() {
+        return null;
+    }
+}
