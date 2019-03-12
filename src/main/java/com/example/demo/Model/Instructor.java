bbef8abbf142c63175d9e2d82744ab397268579e
KatlaEva <orka0002@stud.kea.dk>
2019-03-12 12:38:02 +0100

    Jeg har lavet Instructor class til Model

---
+++ src/main/java/com/example/demo/Model/Instructor.java
@@ -0,0 +1,57 @@
+package com.example.demo.Model;
+
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Instructor {
+
+    @Id
+    private int instructor_id;
+    private String first_name;
+    private String last_name;
+    private String phone_number;
+
+    public Instructor(int instructor_id, String first_name, String last_name, String phone_number) {
+        this.instructor_id = instructor_id;
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.phone_number = phone_number;
+    }
+
+    public Instructor() {
+    }
+
+    public int getInstructor_id() {
+        return instructor_id;
+    }
+
+    public void setInstructor_id(int instructor_id) {
+        this.instructor_id = instructor_id;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getPhone_number() {
+        return phone_number;
+    }
+
+    public void setPhone_number(String phone_number) {
+        this.phone_number = phone_number;
+    }
+}
